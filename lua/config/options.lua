7503a7449c9010d966e1f19074b641a83dcd9924
Shuvo Paul <shuvopaul@yahoo.com>
2025-11-14 11:15:43 +0600

    feat: enable auto import on save in ts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,3 +15,12 @@ vim.api.nvim_create_autocmd("UIEnter", {
     end)
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("organise_imports", { clear = true }),
+  pattern = "*.ts",
+  callback = function()
+    vim.lsp.buf.code_action({ apply = true, context = { only = { "source.addMissingImports.ts" }, diagnostics = {} } })
+    -- vim.lsp.buf.code_action({ apply = true, context = { only = { "source.removeUnused.ts" }, diagnostics = {} } })
+  end,
+})
